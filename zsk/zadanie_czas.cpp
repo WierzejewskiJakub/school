d71ba115ebb1bcf1d95b989b4423e5aa00f05535
WierzejewskiJakub <90031726+WierzejewskiJakub@users.noreply.github.com>
2021-10-01 11:12:03 +0200

    Add files via upload

---
+++ zsk/zadanie_czas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Time {
+	public:
+		static string time;
+		static string date;
+		static void AllData();
+};
+string Time::time
+
+int main(int argc, char** argv) {
+	
+	return 0;
+}
